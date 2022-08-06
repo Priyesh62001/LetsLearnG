b27126963e3751fef311683b653b81945d1ab32d
Priyesh Kumar Pandey <priyeshkumarpandey67@gmail.com>
2022-08-06 08:21:51 +0530

    add message in Sum1.java

--- Sum1.java
+++ Sum1.java
@@ -2,7 +2,7 @@ public class Sum1{
     public static void main(String[] args){
         int a =10;
         int b=20;
-        System.out.println(a+b);
+        System.out.println("This is sum of two number" + (a+b));
 
     }
 }
